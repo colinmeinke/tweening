8198d1aa724bea5be28b886cd13dadfa92017b1e
Colin Meinke <hello@colinmeinke.com>
2016-04-20 18:26:58 -0500

    feat: handle middleware

--- src/tween.js
+++ src/tween.js
@@ -1,17 +1,25 @@
 import easingFuncFromName from './easingFuncFromName';
 import tweenGenerator from './tweenGenerator';
+import { middlewareIn, middlewareOut } from './middleware';
 
-const tween = ({ duration = 200, complete, easing = 'easeInOutQuad', from, next, to }) => {
+const tween = ({ duration = 200, complete, easing = 'easeInOutQuad', from, middleware = [], next, to }) => {
   if ( typeof duration !== 'number' || duration <= 0 || duration == Infinity ) {
     throw new Error( 'Duration must be a positive number less than infinity' );
   }
 
   const easingFunc = typeof easing === 'function' ? easing : easingFuncFromName( easing );
 
-  const g = tweenGenerator({ duration, easing: easingFunc, from, to });
+  const g = tweenGenerator({
+    duration,
+    easing: easingFunc,
+    from: middleware.length ? middlewareIn( from, middleware ) : from,
+    to: middleware.length ? middlewareIn( to, middleware ) : to,
+  });
 
   const animate = () => {
     const { done, value } = g.next();
+
+    const v = middleware.length ? middlewareOut( value, middleware ) : value;
 
     if ( done ) {
       if ( typeof next === 'function' ) {
@@ -23,7 +31,7 @@ const tween = ({ duration = 200, complete, easing = 'easeInOutQuad', from, next,
       }
     } else {
       if ( typeof next === 'function' ) {
-        next( value );
+        next( v );
       }
 
       window.requestAnimationFrame( animate );
